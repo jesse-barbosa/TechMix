ebf3640e539d3e9b752252841e2f263ad4e1f29d
jesse-barbosa <barbosajesse419@gmail.com>
2025-01-23 22:45:52 -0300

    Creating 'Header' and 'Menu' components

---
+++ app/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, TextInput, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Search } from 'lucide-react-native';
+import "@/global.css";
+
+const Header = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View className="component flex-row w-full justify-between items-center p-2">
+      <Image 
+        source={require('@/assets/images/icon.png')} 
+        style={{ height: 45, width: 45 }} 
+        className="mr-2" 
+      />
+      <View className="bg-neutral-800 flex-row justify-between items-center px-3 rounded-lg">
+        <TextInput 
+          placeholder="Buscar Produto ou Loja..." 
+          className="text-white" 
+          style={{ width: '75%' }}
+          placeholderTextColor="#D1D5DB"
+        />
+        <TouchableOpacity onPress={() => (navigation as any).navigate('Search')}>
+          <Search size={26} color="#C0C0C0" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Header;
---
+++ app/components/Menu.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Home, Search, Bookmark, User2 } from 'lucide-react-native';
+import "@/global.css";
+
+const Menu = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const isActive = (routeName: string) => {
+    return route.name === routeName;
+  }
+
+  return (
+    <View className="component flex-row w-full justify-between p-6">
+      <TouchableOpacity onPress={() => (navigation as any).navigate('Home')}>
+        <Home size={32} color={isActive('Home') ? '#fff' : '#C0C0C0'} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => (navigation as any).navigate('Search')}>
+        <Search size={32} color={isActive('Search') ? '#fff' : '#C0C0C0'} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => (navigation as any).navigate('Saves')}>
+        <Bookmark size={32} color={isActive('Saves') ? '#fff' : '#C0C0C0'} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => (navigation as any).navigate('Configs')}>
+        <User2 size={32} color={isActive('Configs') ? '#fff' : '#C0C0C0'} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Menu;
