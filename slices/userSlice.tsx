e337cdf8c6e1190a9d9f345c8193d0f7baf27643
jesse-barbosa <barbosajesse419@gmail.com>
2025-02-14 08:58:38 -0300

    Feat(userSlice): Saving user ID in session

--- slices/userSlice.tsx
+++ slices/userSlice.tsx
@@ -1,11 +1,13 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
+  id: number;
   name: string;
   email: string;
 }
 
 const initialState: UserState = {
+  id: 0,
   name: '',
   email: '',
 };
@@ -15,6 +17,7 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action: PayloadAction<UserState>) => {
+      state.id = action.payload.id;
       state.name = action.payload.name;
       state.email = action.payload.email;
     },
